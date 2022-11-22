1fe8058346b69e4c6de03fe0de04d6f7f6af3a6d
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-11-22 13:54:14 +0700

    init commit

---
+++ src/main/java/com/employeemanagement/clvjava/ClvJavaApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.clvjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClvJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClvJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeemanagement/clvjava/ClvJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.clvjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClvJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
